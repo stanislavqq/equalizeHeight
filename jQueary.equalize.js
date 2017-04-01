0ddf190757061d8b692481800f5a97f41a20bbcd
stanislavqq <stanislavqq@yandex.ru>
2017-04-01 14:07:54 +0300

    Update jQueary.equalize.js
    1.1

--- jQueary.equalize.js
+++ jQueary.equalize.js
@@ -1,23 +1,32 @@
- $.fn.equalizeHeight = function () {
-    return this.each(function () {
-      var $this = $(this),
-              j = 0,
-              children = $this.children();
-      children.css('height', 'auto');
-      $this.children().each(function (i) {
-        if (children.first().offset().top === $(this).offset().top)
-          j++;
-      });
-
-      var rowsCount = Math.ceil(children.length / j);
-      for (var i = 0, elementsCountRow = j, k = 0; i < rowsCount; i++, j = j + elementsCountRow) {
-        var maxHeight = [];
-        var sliceHeights = children.slice(k, j);
-        sliceHeights.each(function () {
-          maxHeight.push($(this).innerHeight());
-        });
-        sliceHeights.height(Math.max.apply(Math, maxHeight));
-        k = k + elementsCountRow;
-      }
-    });
-  };
+(function ($) {
+    var qh = {
+        setHeight: function (jQueryObject, rowsCount, countRowElements) {
+            for (var i = 0, firstRowElement = 0, lastRowElement = countRowElements; i < rowsCount; i++, lastRowElement += countRowElements) {
+                var rowsHeight = []; //Массив со значениями высоты элементов в строке
+                var sliceHeights = jQueryObject.slice(firstRowElement, lastRowElement); //Записываем элементы каждой строки
+                sliceHeights.each(function (i) {
+                    rowsHeight.push($(this).height());
+                });
+                sliceHeights.height(Math.max.apply(Math, rowsHeight)); //Устанавливаем высоту всем элементам строки, выбрав максимальное значение из массива
+                firstRowElement += countRowElements; //устанавливаем индекс первого элемента на следующей строке
+            }
+            return jQueryObject;
+        },
+        getCountRowElements: function (jQueryObject) {
+            var countRowElements = 0;
+            jQueryObject.each(function () {
+                if (jQueryObject.first().offset().top === $(this).offset().top)
+                    ++countRowElements;
+            });
+            return countRowElements;
+        }
+    };
+    $.fn.equalizeHeight = function () {
+        var countRowElements, countRows;
+        this.css('height', 'auto');
+        countRowElements = qh.getCountRowElements(this);
+        countRows = Math.ceil(this.length / countRowElements); //Получаем количество строк
+        qh.setHeight(this, countRows, countRowElements);
+        return this;
+    };
+})(jQuery);
